a3c5d65a69f21328d6e56319de2d0792905e9d3b
Jordi Íñigo Griera <jig@users.noreply.github.com>
2025-12-29 23:13:17 +0100

    Move tests to its own file

---
+++ tests/integration_tests.rs
@@ -0,0 +1,36 @@
+use mal::{initialize_mal_env, mal_env, rep};
+
+#[test]
+fn test_rep_addition() {
+    let env = mal_env();
+    initialize_mal_env(&env, vec![]);
+
+    match rep("(+ 1 1)", &env) {
+        Ok(s) => assert_eq!(s, "2"),
+        Err(_) => panic!("rep() returned an error"),
+    }
+}
+
+#[test]
+fn test_rep_addition_too_many_args() {
+    let env = mal_env();
+    initialize_mal_env(&env, vec![]);
+
+    // Should error because + only accepts 2 arguments
+    match rep("(+ 1 1 1)", &env) {
+        Ok(s) => panic!("Should have returned an error, but got: {}", s),
+        Err(_) => (), // Expected error
+    }
+}
+
+#[test]
+fn test_rep_addition_too_few_args() {
+    let env = mal_env();
+    initialize_mal_env(&env, vec![]);
+
+    // Should error because + requires 2 arguments
+    match rep("(+ 1)", &env) {
+        Ok(s) => panic!("Should have returned an error, but got: {}", s),
+        Err(_) => (), // Expected error
+    }
+}
